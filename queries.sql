97d678578c24cf164c8c4355648f60bae6b715e9
SaneMyburg <sane.myburg@gmail.com>
2023-05-04 06:13:02 +0200

    Use of aggregates functions

--- queries.sql
+++ queries.sql
@@ -63,3 +63,23 @@ WHERE weight_kg < 0;
 
 COMMIT;
 SELECT * FROM animals;
+
+-- Aggregates
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals 
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, MAX(escape_attempts) FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg),
+MAX(weight_kg) from animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
